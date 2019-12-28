4030b64aff5020135042d7fa8beaf8cc3a9e4143
Glenn Latomme <glenn.latomme@gmail.com>
2019-12-28 14:28:51 +0100

    Merge from Master

---
+++ ranking/client/build/precache-manifest.74147c8c7e9190bd29b6ebce25c6de26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3dd20311ee7846187aed8f05836fe54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7912f2e771ffe44c997e",
+    "url": "/static/css/main.0ac9667b.chunk.css"
+  },
+  {
+    "revision": "a50d3320de6b45a99bd3",
+    "url": "/static/js/2.4e9e14f2.chunk.js"
+  },
+  {
+    "revision": "7912f2e771ffe44c997e",
+    "url": "/static/js/main.6fc9d05f.chunk.js"
+  },
+  {
+    "revision": "c089f78d3f04f00dd708",
+    "url": "/static/js/runtime-main.43098ee9.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  }
+]);
